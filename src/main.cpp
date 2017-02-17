2234396a56f8784b398cfeeda983017a51f0e398
lostjared <jaredbruni@gmail.com>
2017-02-16 18:50:25 -0800

    added comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include"qtheaders.h"
 #include "main_window.h"
+#include<unistd.h>
 
 bool blend_set = false;
 cv::Mat blend_image;
 
 int main(int argc, char **argv) {
+    
+
+    /*
+    if(chdir("/usr/share/acidcam") == 0) {
+        std::cout << "Changed directory to: /usr/share/acidcam\n";
+    }
+    */
+    
     QApplication app(argc, argv);
     AC_MainWindow window;
     window.show();
