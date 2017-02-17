a498dae0b62eaf8db0c558967f7e01d1e8a82d12
lostjared <jaredbruni@gmail.com>
2017-02-17 10:23:30 -0800

    forgot to comment out unneeded include

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,12 @@
 #include"qtheaders.h"
 #include "main_window.h"
-#include<unistd.h>
+//#include<unistd.h>
 
 bool blend_set = false;
 cv::Mat blend_image;
 
 int main(int argc, char **argv) {
     
-
     /*
     if(chdir("/usr/share/acidcam") == 0) {
         std::cout << "Changed directory to: /usr/share/acidcam\n";
