a29e41954c3dce5b33e98e20e76d9f3e099d5238
lostjared <jaredbruni@gmail.com>
2018-11-03 00:00:09 -0700

    starting on window

--- src/chroma_window.cpp
+++ src/chroma_window.cpp
@@ -2,3 +2,6 @@
 #include "chroma_window.h"
 
 
+ChromaWindow::ChromaWindow(QWidget *parent) : QDialog(parent) {
+    
+}
--- src/chroma_window.h
+++ src/chroma_window.h
@@ -1,7 +1,14 @@
 #ifndef __CHROMAKEY__H_
 #define __CHROMAKEY__H_
 
+#include "qtheaders.h"
 
+
+class ChromaWindow : public QDialog {
+    Q_OBJECT
+public:
+    ChromaWindow(QWidget *parent);
+};
 
 
 
