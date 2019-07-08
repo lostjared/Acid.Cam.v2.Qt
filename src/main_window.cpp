a1dee9c5371b2748340e6585234a5cb4ef479711
Jared Bruni <jaredbruni@gmail.com>
2019-07-08 07:32:40 -0700

    updated using cast

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -1157,7 +1157,7 @@ QImage Mat2QImage(cv::Mat const& src)
 {
     cv::Mat temp;
     cvtColor(src, temp,cv::COLOR_BGR2RGB);
-    QImage dest((const uchar *) temp.data, temp.cols, temp.rows, temp.step, QImage::Format_RGB888);
+    QImage dest((const uchar *) temp.data, static_cast<int>(temp.cols), static_cast<int>(temp.rows), static_cast<int>(temp.step), QImage::Format_RGB888);
     dest.bits();
     return dest;
 }
