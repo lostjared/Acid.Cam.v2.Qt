ae1c210543810a5275587f1ec02caec7b28fe4de
Jared Bruni <jaredbruni@gmail.com>
2019-09-03 15:01:29 -0700

    added geometry for window

--- src/image_window.cpp
+++ src/image_window.cpp
@@ -1,5 +1,25 @@
 #include "image_window.h"
 
 ImageWindow::ImageWindow(QWidget *parent) : QDialog(parent) {
-    setFixedSize(800, 600);
+    setFixedSize(800, 360);
+    setWindowTitle(tr("Acid Cam v2 - Image Manager"));
+    createControls();
+}
+
+void ImageWindow::createControls() {
+    image_files = new QListWidget(this);
+    image_files->setGeometry(20,20,770,300);
+    add_files = new QPushButton(tr("Add"), this);
+    add_files->setGeometry(15,325,100,25);
+    rmv_file = new QPushButton(tr("Remove"), this);
+    rmv_file->setGeometry(115,325,100,25);
+    set_file = new QPushButton(tr("Set Image"), this);
+    set_file->setGeometry(690,325,100,25);
+    image_cycle_on = new QCheckBox("Image Cycle", this);
+    image_cycle_on->setGeometry(220, 325, 100, 25);
+    image_cycle = new QComboBox(this);
+    image_cycle->setGeometry(320, 325, 100, 25);
+    image_cycle->addItem("Random");
+    image_cycle->addItem("In Order");
+    image_cycle->addItem("Shuffle");
 }
--- src/image_window.h
+++ src/image_window.h
@@ -7,6 +7,12 @@ class ImageWindow : public QDialog {
 Q_OBJECT
 public:
     ImageWindow(QWidget *parent);
+    void createControls();
+private:
+    QListWidget *image_files;
+    QPushButton *add_files, *rmv_file, *set_file;
+    QComboBox *image_cycle;
+    QCheckBox *image_cycle_on;
 };
 
 #endif
