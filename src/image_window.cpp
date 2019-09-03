0e708e7d39884372deefa788b0a6dc14ac92f0e8
Jared Bruni <jaredbruni@gmail.com>
2019-09-03 02:52:20 -0700

    added new window source file and added to pro files

---
+++ src/image_window.cpp
@@ -0,0 +1,5 @@
+#include "image_window.h"
+
+ImageWindow::ImageWindow(QWidget *parent) : QDialog(parent) {
+    
+}
---
+++ src/image_window.h
@@ -0,0 +1,13 @@
+#ifndef __IMAGE_WINDOW__H_
+#define __IMAGE_WINDOW__H_
+
+#include "qtheaders.h"
+
+class ImageWindow : public QDialog {
+Q_OBJECT
+public:
+    ImageWindow(QWidget *parent);
+};
+
+#endif
+
