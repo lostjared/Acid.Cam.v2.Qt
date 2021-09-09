3b30c8b6aa908e1cbab37f241f6ad2adffe4cb88
Jared Bruni <jaredbruni@gmail.com>
2021-09-08 18:22:38 -0700

    removed one

--- src/search_box.cpp
+++ src/search_box.cpp
@@ -17,11 +17,8 @@ SearchWindow::SearchWindow(QWidget *parent) : QDialog(parent) {
 }
 
 bool SearchWindow::checkAdd(QString str) {
-    const char *ex[] = { "AcidShuffleMedian","MatrixColorBlur", "AlphaBlendArrayExpand", "Zoom", "ImageXorSmooth", "SketchFilter", "SlideSub", "Histogram", "Desktop","MultiVideo","Solo", "Bars", "BilateralFilter", "BilateralFilterFade", "BoxFilter", "CurrentDesktopRect", "HorizontalTrailsInter", "IntertwineAlpha", "IntertwineAlphaBlend", "IntertwineVideo640", "RandomAlphaBlendFilter", "RandomOrigFrame", "RectangleGlitch", "SquareSwap64x32", "VideoColorMap", 0};
-
-    
+    const char *ex[] = { "Zoom","AcidShuffleMedian","MatrixColorBlur", "AlphaBlendArrayExpand", "Zoom", "ImageXorSmooth", "SketchFilter", "SlideSub", "Histogram", "Desktop","MultiVideo","Solo", "Bars", "BilateralFilter", "BilateralFilterFade", "BoxFilter", "CurrentDesktopRect", "HorizontalTrailsInter", "IntertwineAlpha", "IntertwineAlphaBlend", "IntertwineVideo640", "RandomAlphaBlendFilter", "RandomOrigFrame", "RectangleGlitch", "SquareSwap64x32", "VideoColorMap", 0};  
     std::string val = str.toStdString();
-    
     for(int i = 0; ex[i] != 0; ++i)
         if(val.find(ex[i]) != std::string::npos)
             return true;
