bb4caeb7efb4923c8e2ee2091d8b81e194f38d83
Jared Bruni <jaredbruni@gmail.com>
2023-02-04 11:10:05 -0800

    updated version info

--- src/qtheaders.h
+++ src/qtheaders.h
@@ -8,7 +8,7 @@
 #define _QT_HEADERS__
 // uncomment below to disable Joystick with libsdl2
 //#define DISABLE_JOYSTICK
-#define ac_version "v1.78.0"
+#define ac_version "v1.80.0"
 #include<QApplication>
 #include<QMainWindow>
 #include<QDialog>
